71d6f870ac55b63b7c3c91461d4b19a7df609b3d
dhruvbhadana4 <dhruvbhadana4@gmail.com>
2026-07-11 16:08:45 +0530

    Time: 0 ms (100%), Space: 26.1 MB (68.71%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,22 +4,23 @@ public:
         int n = nums.size();
         int low = 1;
         int high = n-2;
-        int mid = low + (high - low)/2;
+
+        int mid = low +(high-low)/2;
         if(n==1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        if (nums[0] != nums[1]) return nums[0];
+        else if(nums[n-1]!=nums[n-2]) return nums[n-1];
 
         while(low<=high){
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            else if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]==nums[mid-1])){
-                low = mid + 1;
+            else if((mid%2==0 && nums[mid+1]==nums[mid] )||( mid%2!=0 && nums[mid]==nums[mid-1])){
+                low=mid+1;
             }
             else{
-                high = mid -1;
+                high = mid-1;
             }
-            mid = low + (high - low)/2;
+            mid = low +(high-low)/2;
         }
         return -1;
     }
